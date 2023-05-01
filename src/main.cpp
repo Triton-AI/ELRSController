47c84e2dc630fa0e0b03defa489e36bc40f7094e
Gokul Swaminathan <gokulswamilive@gmail.com>
2023-05-01 16:57:56 -0700

    Change default state
    By default, when there is no connection, pins will be high.

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,51 @@
 FUTABA_SBUS sBus;
 volatile bool noConnection = 0;
 
-void write_to_pins(uint8_t val){
+void writeToAllPins(uint8_t val){
   digitalWrite(5, val);
   digitalWrite(7, val);
   //digitalWrite(8, val);
 }
 
+void setAllPinModes(uint8_t val){
+  pinMode(5, val);
+  pinMode(7, val);
+  //digitalWrite(8, val);
+}
+
 void safetyInterrupt(void){
   if(noConnection){
-    write_to_pins(HIGH);
+    writeToAllPins(HIGH);
   }
   else{
-    write_to_pins(LOW);
+    writeToAllPins(LOW);
   }
   noConnection = true;
 }
 
 void setup() {
-  write_to_pins(OUTPUT);
+  setAllPinModes(OUTPUT);
 
   Timer1.initialize(100000);
   Timer1.attachInterrupt(safetyInterrupt); // check for connection every 0.1 seconds
   noConnection = true;
-  //Set pinout
   sBus.begin();
 }
 
 void loop() {
   sBus.FeedLine();
+
   if (sBus.toChannels == 1){
     sBus.UpdateChannels();
     sBus.toChannels = 0; 
-    if (sBus.channels[4] > 500 || sBus.channels[7] > 500){
-      write_to_pins(HIGH);
-      noConnection = true;
 
+    if (sBus.channels[4] > 500 || sBus.channels[7] > 500){
+      writeToAllPins(HIGH);
+      noConnection = true;
     }
     else{
-      write_to_pins(LOW);
+      writeToAllPins(LOW);
       noConnection = false;
-
     }
   }
 } 
